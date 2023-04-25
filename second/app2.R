f0b8d1c97e63ff0f1287db776b245151b76877f2
michelleg123 <mfg5@geneseo.edu>
2023-04-25 15:15:26 -0400

    hi

---
+++ second/app2.R
@@ -0,0 +1,35 @@
+# New app: 
+
+
+library(shiny)
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(sliderInput("samplesize",
+                             "Sample Size:",
+                             min = 100,
+                             max = 10000,
+                             value = 1000)),
+    mainPanel(plotOutput("distPlot"))
+  )
+) 
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  output$distPlot <- renderPlot({
+    hist(rnorm(input$samplesize),
+         col='darkorchid',
+         xlab="Sample",
+         main="Normally Distributed Sample")},
+    height=300
+  )
+}
+
+
+# Run the application
+shinyApp(ui = ui, server = server)
+
+
+
+
+
